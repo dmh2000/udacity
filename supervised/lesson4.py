f6d19e975e4dbc8f014679d5b2791f9295e9f513
david howard <dmh2000@gmail.com>
2017-07-31 19:52:50 -0700

    added Decision Trees, refactored directories

---
+++ supervised/lesson4.py
@@ -0,0 +1,17 @@
+
+
+m1 = 500.0 / 500.0
+m2 = -200 / 20
+b  = 500
+
+def y(x1,x2):
+    return m1 * x1 + m2 * x2 + b
+
+print(y(500,0))
+print(y(500,20))
+print(y(500,40))
+print(y(1000,0))
+print(y(1000,20))
+print(y(1000,40))
+print m1,m2,b
+
