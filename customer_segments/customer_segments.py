a42bf83d3f2a7b11dc297d86babc73336687d0b9
david howard <dmh2000@gmail.com>
2017-08-28 17:12:23 -0700

    clustering done

--- customer_segments/customer_segments.py
+++ customer_segments/customer_segments.py
@@ -124,9 +124,63 @@ pca_samples = pca.transform(log_samples)
 # Generate PCA results plot
 pca_results = vs.pca_results(good_data, pca)
 
-print 0.4993
-component 2 variance  = 0.2259
-component 3 variance  = 0.1049
-component 4 variance  = 0.0978
+# TODO: Apply PCA by fitting the good data with only two dimensions
+pca = PCA(n_components=2, random_state=42)
 
-print 0.4993 + 0.2259 + 0.1049 + 0.0978
+# TODO: Transform the good data using the PCA fit above
+reduced_data = pca.fit_transform(good_data)
+
+# TODO: Transform log_samples using the PCA fit above
+pca_samples = pca.transform(log_samples)
+
+# Create a DataFrame for the reduced data
+reduced_data = pd.DataFrame(reduced_data, columns = ['Dimension 1', 'Dimension 2'])
+
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from sklearn.mixture import GaussianMixture
+
+# Apply your clustering algorithm of choice to the reduced data
+clusterer = KMeans(n_clusters=2, random_state=2)
+
+clusterer.fit(reduced_data)
+
+# Predict the cluster for each data point
+preds = clusterer.predict(reduced_data)
+
+# TODO: Find the cluster centers
+centers = clusterer.cluster_centers_
+print(centers)
+# TODO: Predict the cluster for each transformed sample data point
+sample_preds = clusterer.predict(pca_samples)
+
+# TODO: Calculate the mean silhouette coefficient for the number of clusters chosen
+score = silhouette_score(reduced_data,preds)
+
+print(score)
+
+# n_clusters = 2 0.447157742293
+# n_clusters = 3 0.364874035612
+# n_clusters = 4 0.331150954285
+# n_clusters = 5 0.350794538773
+
+
+
+# Apply your clustering algorithm of choice to the reduced data
+clusterer = GaussianMixture(n_components=2)
+
+clusterer.fit(reduced_data)
+
+# Predict the cluster for each data point
+preds = clusterer.predict(reduced_data)
+
+# Find the cluster centers
+centers = clusterer.means_
+
+# Predict the cluster for each transformed sample data point
+sample_preds = clusterer.predict(pca_samples)
+
+# Calculate the mean silhouette coefficient for the number of clusters chosen
+score = silhouette_score(reduced_data,preds)
+
+print(score)
