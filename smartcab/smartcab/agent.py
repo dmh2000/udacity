2076b0ae3286119a12820ff6b06248c443223253
david howard <dmh2000@gmail.com>
2017-09-14 16:14:16 -0700

    partial smartcab

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -1,29 +1,32 @@
 import random
 import math
+import numpy as np
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
 
+
 class LearningAgent(Agent):
     """ An agent that learns to drive in the Smartcab world.
-        This is the object you will be modifying. """ 
+        This is the object you will be modifying. """
 
-    def __init__(self, env, learning=False, epsilon=1.0, alpha=0.5):
-        super(LearningAgent, self).__init__(env)     # Set the agent in the evironment 
+    def __init__(self, env, learning=False, epsilon=1.0, alpha=0.5, testing=False, epsilon_decay=.99):
+        super(LearningAgent, self).__init__(env)  # Set the agent in the evironment
         self.planner = RoutePlanner(self.env, self)  # Create a route planner
         self.valid_actions = self.env.valid_actions  # The set of valid actions
 
         # Set parameters of the learning agent
-        self.learning = learning # Whether the agent is expected to learn
-        self.Q = dict()          # Create a Q-table which will be a dictionary of tuples
-        self.epsilon = epsilon   # Random exploration factor
-        self.alpha = alpha       # Learning factor
+        self.learning = learning  # Whether the agent is expected to learn
+        self.Q = dict()  # Create a Q-table which will be a dictionary of tuples
+        self.epsilon = epsilon  # Random exploration factor
+        self.alpha = alpha  # Learning factor
 
         ###########
         ## TO DO ##
         ###########
         # Set any additional class parameters as needed
-
+        self.epsilon_decay = epsilon_decay  # decay rate of epsilon (simple version)
+        self.testing = testing
 
     def reset(self, destination=None, testing=False):
         """ The reset function is called at the beginning of each trial.
@@ -32,13 +35,18 @@ class LearningAgent(Agent):
 
         # Select the destination as the new location to route to
         self.planner.route_to(destination)
-        
+
         ########### 
         ## TO DO ##
         ###########
         # Update epsilon using a decay function of your choice
         # Update additional class parameters as needed
         # If 'testing' is True, set epsilon and alpha to 0
+        if self.testing:
+            self.epsilon = 0.0
+            self.alpha   = 0.0
+        else:
+            self.epsilon = self.epsilon * self.epsilon_decay
 
         return None
 
@@ -48,24 +56,41 @@ class LearningAgent(Agent):
             are all features available to the agent. """
 
         # Collect data about the environment
-        waypoint = self.planner.next_waypoint() # The next waypoint 
-        inputs = self.env.sense(self)           # Visual input - intersection light and traffic
+        waypoint = self.planner.next_waypoint()  # The next waypoint
+        inputs = self.env.sense(self)  # Visual input - intersection light and traffic
         deadline = self.env.get_deadline(self)  # Remaining deadline
 
         ########### 
         ## TO DO ##
         ###########
-        
-        # NOTE : you are not allowed to engineer eatures outside of the inputs available.
+
+        # NOTE : you are not allowed to engineer features outside of the inputs available.
         # Because the aim of this project is to teach Reinforcement Learning, we have placed 
         # constraints in order for you to learn how to adjust epsilon and alpha, and thus learn about the balance between exploration and exploitation.
         # With the hand-engineered features, this learning process gets entirely negated.
-        
-        # Set 'state' as a tuple of relevant data for the agent        
-        state = None
+
+        # Set 'state' as a tuple of relevant data for the agent
+        print(inputs)
+
+        # valid = False
+        # # right on green is always ok
+        # if waypoint == 'right' and inputs['light'] == 'green':
+        #     valid_action = True
+        # # right on red is ok if left is turning left
+        # elif waypoint == 'right' and inputs['light'] == 'red' and inputs['left'] == 'left':
+        #     valid_action = True
+        # # right on red is ok if left is turning right
+        # elif waypoint == 'right' and inputs['light'] == 'red' and inputs['left'] == 'right':
+        #     valid_action = True
+        # # forward on green is always ok
+        # elif waypoint == 'forward' and inputs['light'] == 'green':
+        #     valid_action = True
+        # elif waypoint == 'left' and inputs['light'] == 'green' and inputs['incoming'] != 'forward':
+        #     valid_action = True
+
+        state = (waypoint, inputs['light'], inputs['left'], inputs['oncoming'])
 
         return state
-
 
     def get_maxQ(self, state):
         """ The get_max_Q function is called when the agent is asked to find the
@@ -78,8 +103,7 @@ class LearningAgent(Agent):
 
         maxQ = None
 
-        return maxQ 
-
+        return maxQ
 
     def createQ(self, state):
         """ The createQ function is called when a state is generated by the agent. """
@@ -90,9 +114,15 @@ class LearningAgent(Agent):
         # When learning, check if the 'state' is not in the Q-table
         # If it is not, create a new dictionary for that state
         #   Then, for each action available, set the initial Q-value to 0.0
+        if state not in self.Q:
+            self.Q[state] = {
+                'left' : 0.0,
+                'right': 0.0,
+                'forward': 0.0,
+                'wait': 0.0
+            }
 
         return
-
 
     def choose_action(self, state):
         """ The choose_action function is called when the agent is asked to choose
@@ -106,12 +136,30 @@ class LearningAgent(Agent):
         ########### 
         ## TO DO ##
         ###########
-        # When not learning, choose a random action
-        # When learning, choose a random action with 'epsilon' probability
-        # Otherwise, choose an action with the highest Q-value for the current state
-        # Be sure that when choosing an action with highest Q-value that you randomly select between actions that "tie".
-        return action
+        if not self.testing:
+            if self.learning:
+                # When learning, choose a random action with 'epsilon' probability
+                action = np.random.choice(self.valid_actions)
+                pass
+            else:
+                # When not learning, choose a random action
+                action = np.random.choice(self.valid_actions)
+        else:
+            # Otherwise, choose an action with the highest Q-value for the current state
+            # Be sure that when choosing an action with highest Q-value that you randomly select between actions that "tie".
+            q = self.Q[state]
+            a = []
+            mq = 0
+            # get array of max q values
+            for key, value in q:
+                if value > mq:
+                    mq = value
+                    a.append(key)
+            # select one at random
+            action = np.random.choice(a)
 
+
+        return action
 
     def learn(self, state, action, reward):
         """ The learn function is called after the agent completes an action and
@@ -126,20 +174,19 @@ class LearningAgent(Agent):
 
         return
 
-
     def update(self):
         """ The update function is called when a time step is completed in the 
             environment for a given trial. This function will build the agent
             state, choose an action, receive a reward, and learn if enabled. """
 
-        state = self.build_state()          # Get current state
-        self.createQ(state)                 # Create 'state' in Q-table
+        state = self.build_state()  # Get current state
+        self.createQ(state)  # Create 'state' in Q-table
         action = self.choose_action(state)  # Choose an action
-        reward = self.env.act(self, action) # Receive a reward
-        self.learn(state, action, reward)   # Q-learn
+        reward = self.env.act(self, action)  # Receive a reward
+        self.learn(state, action, reward)  # Q-learn
 
         return
-        
+
 
 def run():
     """ Driving function for running the simulation. 
@@ -152,7 +199,7 @@ def run():
     #   num_dummies - discrete number of dummy agents in the environment, default is 100
     #   grid_size   - discrete number of intersections (columns, rows), default is (8, 6)
     env = Environment()
-    
+
     ##############
     # Create the driving agent
     # Flags:
@@ -160,12 +207,12 @@ def run():
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
     agent = env.create_agent(LearningAgent)
-    
+
     ##############
     # Follow the driving agent
     # Flags:
     #   enforce_deadline - set to True to enforce a deadline metric
-    env.set_primary_agent(agent)
+    env.set_primary_agent(agent, enforce_deadline=True)
 
     ##############
     # Create the simulation
@@ -174,15 +221,18 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env)
-    
+    sim = Simulator(env, update_delay=0.1, log_metrics=True)
+
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run()
+    sim.run(n_test=10)
 
 
 if __name__ == '__main__':
+    # seed the random number generator to get consistent results
+    np.random.seed(41)
+    # run the simulation
     run()
