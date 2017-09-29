f0b202ebbdecf854eec4b24b592a553dc5d0d2e9
david howard <dmh2000@gmail.com>
2017-09-28 17:46:17 -0700

    partial dog project
    Former-commit-id: 97a905eb547ea2bd27da0103b24dec0ca825f490

--- dog-project/dog_app.py
+++ dog-project/dog_app.py
@@ -26,3 +26,105 @@ print('There are %s total dog images.\n' % len(np.hstack([train_files, valid_fil
 print('There are %d training dog images.' % len(train_files))
 print('There are %d validation dog images.' % len(valid_files))
 print('There are %d test dog images.' % len(test_files))
+
+import random
+
+random.seed(8675309)
+
+# load filenames in shuffled human dataset
+human_files = np.array(glob("lfw/*/*"))
+random.shuffle(human_files)
+
+# print statistics about the dataset
+print('There are %d total human images.' % len(human_files))
+
+import cv2
+import matplotlib.pyplot as plt
+
+# % matplotlib inline
+
+# extract pre-trained face detector
+face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
+
+# load color (BGR) image
+img = cv2.imread(human_files[3])
+# convert BGR image to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# find faces in image
+faces = face_cascade.detectMultiScale(gray)
+
+# print number of faces detected in the image
+print('Number of faces detected:', len(faces))
+
+# get bounding box for each detected face
+for (x, y, w, h) in faces:
+    # add bounding box to color image
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+# convert BGR image to RGB for plotting
+cv_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+# display the image, along with bounding box
+# plt.imshow(cv_rgb)
+# plt.show()
+
+
+# returns "True" if face is detected in image stored at img_path
+def face_detector(img_path):
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    return len(faces) > 0
+
+
+human_files_short = human_files[:100]
+dog_files_short = train_files[:100]
+# Do NOT modify the code above this line.
+
+# Test the performance of the face_detector algorithm
+# on the images in human_files_short and dog_files_short.
+# humans = 0
+# for h in human_files_short:
+#     isface = face_detector(h)
+#     if isface:
+#         humans += 1
+#
+# dogs = 0
+# for d in dog_files_short:
+#     isface = face_detector(d)
+#     if isface:
+#         dogs += 1
+#
+# print(humans, dogs)
+
+
+# import dlib
+# from skimage import io
+#
+#
+# def dlib_face(img_path):
+#     detector = dlib.get_frontal_face_detector()
+#     img = io.imread(img_path)
+#     dets = detector(img, 1)
+#     return len(dets) > 0
+#
+#
+# humans = 0
+# for h in human_files_short:
+#     isface = dlib_face(h)
+#     if isface:
+#         humans += 1
+#
+# dogs = 0
+# for d in dog_files_short:
+#     isface = dlib_face(d)
+#     if isface:
+#         dogs += 1
+# print(humans,dogs)
+
+from keras.applications.resnet50 import ResNet50
+
+# define ResNet50 model
+ResNet50_model = ResNet50(weights='imagenet')
